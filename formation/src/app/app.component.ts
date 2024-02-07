39f08baea9a1c7dd1af184fae070f0c86f1b0405
Absink <absink@absink.fr>
2024-02-07 13:44:00 +0100

    Add icon-login + navbar component

--- formation/src/app/app.component.ts
+++ formation/src/app/app.component.ts
@@ -3,11 +3,12 @@ import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { SharedModule } from './shared/shared.module';
 import { HeaderComponent } from './core/components/header/header.component';
+import { NavbarComponent } from './core/components/navbar/navbar.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, CommonModule, SharedModule, HeaderComponent],
+  imports: [RouterOutlet, CommonModule, SharedModule, HeaderComponent, NavbarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
---
+++ formation/src/app/shared/components/icon-login/icon-login.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-icon-login',
+  templateUrl: './icon-login.component.html',
+  styleUrl: './icon-login.component.scss'
+})
+export class IconLoginComponent {
+
+}
