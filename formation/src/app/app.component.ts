0a41a4c8a034bf7b6b82ae84a94be1c4eff27c89
Absink <absink@absink.fr>
2024-02-07 14:29:36 +0100

    interpolation + event binding

--- formation/src/app/app.component.ts
+++ formation/src/app/app.component.ts
@@ -15,10 +15,20 @@ import { NavbarComponent } from './core/components/navbar/navbar.component';
 export class AppComponent implements OnInit {
 
   public open: boolean = false;
-  title = 'formation';
+  public msgConnexion: string | undefined;
+
+  constructor() { }
 
   ngOnInit(): void {
     this.open = false;
+    this.msgConnexion = "Non conencté";
+  }
+
+  public login(): void {
+    if (this.msgConnexion == "Non conencté")
+      this.msgConnexion = "Bienvenue";
+    else
+      this.msgConnexion = "Non conencté"
   }
 
   public switch(): void {
