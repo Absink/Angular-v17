75917c9222526f61d42e4d94682a764672d0b7fe
Absink <absink@absink.fr>
2024-02-08 10:51:30 +0100

    Add models, httpClientModule, Observables, services

--- formation/src/app/features/order/page-list-orders/page-list-orders.component.ts
+++ formation/src/app/features/order/page-list-orders/page-list-orders.component.ts
@@ -1,12 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Order } from '../../../shared/models/order.model';
+import { OrderService } from '../../../shared/services/order.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-page-list-orders',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './page-list-orders.component.html',
   styleUrl: './page-list-orders.component.scss'
 })
-export class PageListOrdersComponent {
+export class PageListOrdersComponent implements OnInit {
+
+  public listOrder!: Order[];
+
+  constructor(private orderService: OrderService) { }
+
+
+  ngOnInit(): void {
+    this.orderService.collection.subscribe(datas => {
+      this.listOrder = datas;
+    });
+    console.log(this.listOrder)
+  }
 
 }
---
+++ formation/src/app/shared/services/order.service.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Order } from '../models/order.model';
+import { environment } from '../../../environments/environment.development';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class OrderService {
+
+  private pCollection!: Observable<Order[]>;
+  private api: string = environment.api;
+
+  constructor(private http: HttpClient) {
+    this.collection = this.http.get<Order[]>(`${this.api}/orders`);
+  }
+
+  // Getters / Setters
+  get collection(): Observable<Order[]> {
+    return this.pCollection;
+  }
+
+  set collection(col: Observable<Order[]>) {
+    this.pCollection = col;
+  }
+}
